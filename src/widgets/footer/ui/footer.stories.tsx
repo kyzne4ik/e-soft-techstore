a087f0241903c114d995b756a92adb3f1be608da
Никита Тимофеев <freemorobskyr@gmail.com>
2026-04-20 00:45:10 +0500

    добавил футер

---
+++ src/widgets/footer/ui/footer.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Footer } from "./footer";
+
+const meta: Meta<typeof Footer> = {
+  title: "widgets/Footer",
+  component: Footer,
+  parameters: {
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
