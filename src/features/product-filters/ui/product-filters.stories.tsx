61904390bb27681a03cde6b3e8c6f89e5911b431
Никита Тимофеев <freemorobskyr@gmail.com>
2026-04-21 15:19:14 +0500

    Удалил ненужный.gitkeep фикс импорта в *.stories'ах

--- src/features/product-filters/ui/product-filters.stories.tsx
+++ src/features/product-filters/ui/product-filters.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ProductFilters } from "./product-filters";
 
 const meta: Meta<typeof ProductFilters> = {
