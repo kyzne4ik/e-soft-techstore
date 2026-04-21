fcc85f45667bdf0e81689f2cfc098fcd45becf70
Никита Тимофеев <freemorobskyr@gmail.com>
2026-04-21 12:52:35 +0500

    добавил строгий контекст
    createStrictContext
    useStrictContext

---
+++ src/shared/lib/hooks/react.ts
@@ -0,0 +1,14 @@
+import type { Context } from "react";
+import { createContext, useContext } from "react";
+
+export function useStrictContext<T>(context: Context<T | null>) {
+  const value = useContext(context);
+  if (value === null) {
+    throw new Error("Strict context not passed");
+  }
+  return value;
+}
+
+export function createStrictContext<T>() {
+  return createContext<T | null>(null);
+}
