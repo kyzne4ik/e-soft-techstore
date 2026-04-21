5d844ded40912fa99f8c099070616908d19082f8
Никита Тимофеев <freemorobskyr@gmail.com>
2026-04-21 12:49:33 +0500

    добавил контракты для product'ов

--- src/shared/api/contracts/products.contract.ts
+++ src/shared/api/contracts/products.contract.ts
@@ -1,9 +1,16 @@
-import type { Product } from "~entities/product";
+export type ProductCategory = "tv" | "phone" | "laptop";
 
-export interface GetProductsParams {
+export type ProductDto = {
+  id: number;
+  category: ProductCategory;
+  make: string;
+  model: string;
+  price: number;
+  images: string[];
+  isSpecialOffer?: boolean;
+  brand: string;
+};
+
+export type GetProductsParams = {
   category?: string;
-}
-
-export interface ProductsResponse {
-  products: Product[];
-}
+};
