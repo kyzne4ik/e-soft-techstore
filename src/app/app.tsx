324b19f0fd677f3e4be47ae3e4706eb4551505fb
Никита Тимофеев <freemorobskyr@gmail.com>
2026-04-21 14:16:17 +0500

    добавил по тз в корневой компонент:
    <Container>
    <Header/>
    <Content/>
    <Footer/>
    </Container>

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,70 @@
-import { HomePage } from "~pages/home";
-import "./app.css";
+import { useState } from "react";
+import { Header } from "~widgets/header";
+import { Footer } from "~widgets/footer";
+import { ProductList } from "~widgets/product-list";
+import { ProductFiltersSidebar } from "~widgets/product-filters-sidebar";
+import { RegistrationBannerWidget } from "~widgets/registration-banner";
+import { CartPage } from "~pages/cart";
+import styles from "./app.module.css";
+import { useContainer } from "@/shared/lib/context/container";
+import { Container } from "./providers/container-provider";
 
-function App() {
-  return <HomePage />;
+export default function App() {
+  return (
+    <Container>
+      <BaseApp />
+    </Container>
+  );
 }
 
-export default App;
+function BaseApp() {
+  const { pageType, setPageType, cart } = useContainer();
+
+  const cartItemsCount = Object.values(cart).reduce((sum, qty) => sum + qty, 0);
+
+  return (
+    <div className={styles.page}>
+      <Header
+        cartItemsCount={cartItemsCount}
+        activeCategory={pageType}
+        onCategoryChange={(category) => setPageType(category)}
+      />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+function Content() {
+  const { pageType } = useContainer();
+
+  const [filters, setFilters] = useState({
+    brand: "",
+    minPrice: 0,
+    maxPrice: 5000,
+  });
+
+  if (pageType === "cart") {
+    return (
+      <main className={styles.main}>
+        <CartPage />
+      </main>
+    );
+  }
+
+  return (
+    <main className={styles.main}>
+      <aside className={styles.sidebar}>
+        <ProductFiltersSidebar
+          key={pageType}
+          category={pageType}
+          onApply={setFilters}
+        />
+        <RegistrationBannerWidget />
+      </aside>
+      <section className={styles.content}>
+        <ProductList key={pageType} category={pageType} filters={filters} />
+      </section>
+    </main>
+  );
+}
