b1e4207b8edd975665eeb367dbab8af884ff7a09
Никита Тимофеев <freemorobskyr@gmail.com>
2026-04-21 13:10:22 +0500

    убрал из product-list-header'а опции (Featured&Newest)
    + косметика

--- src/features/product-sort/ui/product-list-header.stories.tsx
+++ src/features/product-sort/ui/product-list-header.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ProductListHeader } from "./product-list-header";
 
 const meta: Meta<typeof ProductListHeader> = {
